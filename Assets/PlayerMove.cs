551c7b0a14e44d4db302440017dc7308fc8f94bf
kimgunwooo <kwforu123@naver.com>
2023-06-11 22:48:59 +0900

    Revert "VR 작업 - main"
    This reverts commit d551cf39c6e985d80689342b9d60e3f83bcb99c4.

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Speed
+    // 이동속도
     public float speed = 5;
-
-    // CharacterController component
+    // CharacterController 컴포넌트
     CharacterController cc;
 
-    // Magnitude of gravitational acceleration
+    // 중력 가속도의 크기
     public float gravity = -20;
-    // Vertical speed
+    // 수직 속도
     float yVelocity = 0;
 
-    // Jump size
+    // 점프 크기
     public float jumpPower = 5;
-
-    // Rotation speed for player and camera
-    public float rotationSpeed = 5;
 
     void Start()
     {
@@ -29,32 +25,29 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get user input for movement
-        float hMovement = ARAVRInput.GetAxis("Horizontal");
-        float vMovement = ARAVRInput.GetAxis("Vertical");
-
-
-        // Create movement direction
-        Vector3 moveDirection = new Vector3(hMovement, 0, vMovement);
-        // Change the input value in the direction the user is looking
-        moveDirection = Camera.main.transform.TransformDirection(moveDirection);
-        // Add vertical direction with gravity applied (v = v0 + at)
+        // 사용자의 입력에 따라 전후좌우로 이동하고 싶다.
+        // 1. 사용자의 입력을 받는다.
+        float h = ARAVRInput.GetAxis("Horizontal");
+        float v = ARAVRInput.GetAxis("Vertical");
+        // 2. 방향을 만든다.
+        Vector3 dir = new Vector3(h, 0, v);
+        // 2.0 사용자가 바라보는 방향으로 입력 값 변화시키기
+        dir = Camera.main.transform.TransformDirection(dir);
+        // 2.1 중력을 적용한 수직 방향 추가 v=v0+at
         yVelocity += gravity * Time.deltaTime;
-        // When on the floor, zero velocity to account for normal drag
+        //2.2 바닥에 있을 경우, 수직 항력을 처리하기 위해 속도를 0으로 한다.
         if (cc.isGrounded)
         {
             yVelocity = 0;
         }
-        // Assign a jump size when the user presses the jump button
+        // 2.3 사용자가 점프 버튼을 누르면 점프 크기를 할당한다.
         if (ARAVRInput.GetDown(ARAVRInput.Button.Two, ARAVRInput.Controller.RTouch))
         {
             yVelocity = jumpPower;
         }
 
-        moveDirection.y = yVelocity;
-
-        // Move the player
-        cc.Move(moveDirection * speed * Time.deltaTime);
-
+        dir.y = yVelocity;
+        // 3. 이동한다
+        cc.Move(dir * speed * Time.deltaTime);
     }
 }
